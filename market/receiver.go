cb9c0513b09bc552061b3b996f8266caf7c8f2c1
dc-dc-dc <dgcruz983@gmail.com>
2023-11-24 11:40:01 -0500

    functional receiver

--- market/receiver.go
+++ market/receiver.go
@@ -10,3 +10,15 @@ type CachableReceiver interface {
 	CacheKey() string
 	MarketReceiver
 }
+
+type FunctionalReceiver struct {
+	call func(context.Context, MarketLine) error
+}
+
+func NewFunctionalReceiver(call func(context.Context, MarketLine) error) MarketReceiver {
+	return &FunctionalReceiver{call: call}
+}
+
+func (r *FunctionalReceiver) Receive(ctx context.Context, line MarketLine) error {
+	return r.call(ctx, line)
+}
