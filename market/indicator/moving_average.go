3f00a8b8e7b8b39fe3e7dba1ee11ec04bf7488b8
dc-dc-dc <dgcruz983@gmail.com>
2023-11-23 20:31:28 -0500

    added exponential moving average

--- market/indicator/moving_average.go
+++ market/indicator/moving_average.go
@@ -8,21 +8,55 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-var _ market.MarketReceiver = (*SimpleMovingAverage)(nil)
+var _ market.MarketReceiver = (*MovingAverage)(nil)
+var _ MovingAverageCalc = SimpleMovingAverageCalc
 
-const ContextIndicatorSimpleMovingAverage = "indicator.simple_moving_average"
+const ContextIndicatorSimpleMovingAverage = "indicator.moving_average"
 
-type SimpleMovingAverage struct {
-	queue *util.CappedQueue
+type MovingAverageCalc func([]decimal.Decimal) decimal.Decimal
+
+func SimpleMovingAverageCalc(items []decimal.Decimal) decimal.Decimal {
+	var sum decimal.Decimal
+	for _, element := range items {
+		sum = sum.Add(element)
+	}
+	return sum.Div(decimal.NewFromInt(int64(len(items))))
 }
 
-func NewSimpleMovingAverage(count int) *SimpleMovingAverage {
-	return &SimpleMovingAverage{
+func ExponentialMovingAverageCalc(items []decimal.Decimal) decimal.Decimal {
+	var sum decimal.Decimal
+	k := decimal.NewFromFloat32(2.0 / float32(len(items)+1))
+	for i, element := range items {
+		if i == 0 {
+			sum = element
+		} else {
+			sum = element.Mul(k).Add(sum.Mul(decimal.NewFromFloat32(1.0).Sub(k)))
+		}
+	}
+	return sum
+}
+
+type MovingAverage struct {
+	queue *util.CappedQueue
+	calc  MovingAverageCalc
+}
+
+func NewSimpleMovingAverage(count int) *MovingAverage {
+	return NewMovingAverage(count, SimpleMovingAverageCalc)
+}
+
+func NewExponentialMovingAverage(count int) *MovingAverage {
+	return NewMovingAverage(count, ExponentialMovingAverageCalc)
+}
+
+func NewMovingAverage(count int, calc MovingAverageCalc) *MovingAverage {
+	return &MovingAverage{
 		queue: util.NewCappedQueue(count),
+		calc:  calc,
 	}
 }
 
-func (sa *SimpleMovingAverage) Receive(ctx context.Context, line market.MarketLine) error {
+func (sa *MovingAverage) Receive(ctx context.Context, line market.MarketLine) error {
 	cache, ok := ctx.Value(market.ContextCache).(map[string]interface{})
 	if !ok {
 		return market.ErrNoContextCache
@@ -30,13 +64,11 @@ func (sa *SimpleMovingAverage) Receive(ctx context.Context, line market.MarketLi
 
 	sa.queue.Push(line.Close)
 	if sa.queue.Full() {
-		elements := sa.queue.Elements()
-		var sum decimal.Decimal
-		for _, element := range elements {
-			sum = sum.Add(element.(decimal.Decimal))
+		items := make([]decimal.Decimal, sa.queue.Cap())
+		for i, t := range sa.queue.Elements() {
+			items[i] = t.(decimal.Decimal)
 		}
-		average := sum.Div(decimal.NewFromInt(int64(sa.queue.Cap())))
-		cache[ContextIndicatorSimpleMovingAverage] = average
+		cache[ContextIndicatorSimpleMovingAverage] = sa.calc(items)
 	}
 	return nil
 }
