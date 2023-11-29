910672f5ecd8fed624c497ff84fff33aeacfd8df
diogo cruz <dgcruz983@gmail.com>
2023-11-28 20:27:05 -0500

    small refactor to yfinance

--- market/csv/yfinance.go
+++ market/csv/yfinance.go
@@ -3,6 +3,7 @@ package csv
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 
@@ -25,22 +26,32 @@ func NewYFinanceProducer(symbol string, interval market.Interval, start, end tim
 	}
 }
 
-func (p *yFinanceProducer) Produce(ctx context.Context, out chan market.MarketLine) error {
+func (p *yFinanceProducer) Fetch(ctx context.Context) (io.ReadCloser, error) {
 	url := fmt.Sprintf("https://query1.finance.yahoo.com/v7/finance/download/%s?period1=%d&period2=%d&interval=%s&events=history", p.symbol, p.start.Unix(), p.end.Unix(), p.interval.String())
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	if res.StatusCode != http.StatusOK {
-		return fmt.Errorf("status code: %d", res.StatusCode)
+		return nil, fmt.Errorf("status code: %d", res.StatusCode)
 	}
+	return res.Body, nil
+}
 
-	defer res.Body.Close()
-	// provide the symbol in the context, any producers that care will use it
+func (p *yFinanceProducer) String() string {
+	return fmt.Sprintf("yFinanceProducer{symbol: %s, interval: %s, start: %s, end: %s}", p.symbol, p.interval, p.start, p.end)
+}
+
+func (p *yFinanceProducer) Produce(ctx context.Context, out chan market.MarketLine) error {
+	res, err := p.Fetch(ctx)
+	if err != nil {
+		return err
+	}
+	defer res.Close()
 	ctx = context.WithValue(ctx, market.ContextKeySymbol, p.symbol)
-	return NewCsvProducer(res.Body).Produce(ctx, out)
+	return NewCsvProducer(res).Produce(ctx, out)
 }
